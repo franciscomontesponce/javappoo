53c506d381610d1a9aee2d73d1017691797db5a6
Francisco Montes <franciscomontesmkt@gmail.com>
2023-03-08 23:52:02 -0300

    Actualización de archivos

--- src/PruebaMetodos.java
+++ src/PruebaMetodos.java
@@ -1,7 +1,13 @@
 public class PruebaMetodos {
     public static void main (String[] args){
+
+        System.out.println("");
         
         Cuenta miCuenta = new Cuenta();
         System.out.println(miCuenta);
+
+        miCuenta.saldo = 300;
+        System.out.println(miCuenta.saldo);
+
     }
 }
