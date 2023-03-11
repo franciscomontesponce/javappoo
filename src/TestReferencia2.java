7e366d737a5e930c371e2dc5d3f748bf9deb4cca
Francisco Montes <franciscomontesmkt@gmail.com>
2023-03-10 21:23:20 -0300

    Se ingresa archivo TestReferencia2

---
+++ src/TestReferencia2.java
@@ -0,0 +1,9 @@
+public class TestReferencia2 {
+    
+    public static void main(String[] args) {
+        Cliente diego = new Cliente();
+        diego.nombre = "Diego";
+        diego.documento = "123456789";
+        diego.telefono = "55559999";
+    }
+}
